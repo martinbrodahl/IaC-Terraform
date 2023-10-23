8a817835dc8b148289bb53f2271e63cd9a81a99b
martbrod <martbrod@stud.ntnu.no>
2023-10-23 16:30:16 +0200

    edited main (sa name)

--- Course materials/module06/week40exercises/main.tf
+++ Course materials/module06/week40exercises/main.tf
@@ -31,7 +31,7 @@ locals {
   workspaces_suffix = terraform.workspace == "default" ? "" : "${terraform.workspace}"
 
   rg_web_name = terraform.workspace == "default" ? "${var.rg_web_name}" : "${var.rg_web_name}-${local.workspaces_suffix}"
-  sa_web_name = terraform.workspace == "default" ? "${var.sa_web_name}" : "${var.sa_web_name}-${local.workspaces_suffix}"
+  sa_web_name = terraform.workspace == "default" ? "${var.sa_web_name}" : "${var.sa_web_name}${local.workspaces_suffix}"
 
   source_content = "${var.source_content}<p>${local.workspaces_suffix}<p>"
 }
