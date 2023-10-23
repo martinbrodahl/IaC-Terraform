5b554b4c5f3ca2eadafea8128b117644436e4fd4
martbrod <martbrod@stud.ntnu.no>
2023-10-23 14:45:30 +0200

    oppdatering for deploy

--- Course materials/module06/week40exercises/variables.tf
+++ Course materials/module06/week40exercises/variables.tf
@@ -2,11 +2,13 @@
 variable "rg_web_name" {
   type        = string
   description = "Static website resource group name"
+  default = "rg-web-week40"
 }
 
 variable "rg_web_location" {
   type        = string
   description = "Static website rg location"
+  default = "westeurope"
 }
 
 
@@ -14,11 +16,13 @@ variable "rg_web_location" {
 variable "sa_web_name" {
   type        = string
   description = "(optional) describe your variable"
+  default = "sawebweek40"
 }
 
 variable "source_content" {
   type        = string
   description = "Source content for the index.html file"
+  default = "<h1>Web page created with Terraform</h1>"
 }
 
 
@@ -26,4 +30,5 @@ variable "source_content" {
 variable "index_document" {
   type        = string
   description = "Name of the index document"
+  default = "index.html"
 }
